5cc2deced7b4a1e18a5723153cca0ffe40687374
AliceChpmnk <alisablog50@gmail.com>
2023-03-20 01:21:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
-    // перед вивантаженням на гітхаб
-    // basename="/goit-react-homework-05/"
-    >
+    <BrowserRouter basename="/goit-react-homework-05/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
